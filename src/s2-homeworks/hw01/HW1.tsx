65c05c42cfb42299026c5456124961705c1b152f
Troan89 <wiki-mars@yandex.ru>
2023-06-21 20:36:02 +0400

    Стилизация сообщений и типизация

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -14,17 +14,31 @@ import avatar from './avatar.png'
 * */
 
 // нужно создать правильный тип вместо any
-export type MessageType = any
+
+export type MessagesType ={
+    text: string,
+    time: string
+}
+export type UserType ={
+    avatar: string,
+    name: string
+}
+
+export type MessageType = {
+    id: number,
+    user: UserType
+    message: MessagesType
+}
 
 // структуру объекта не менять
 export const message0: MessageType = {
     id: 0,
     user: {
         avatar: avatar, // можно менять
-        name: 'Some Name',  // можно менять
+        name: 'Ivan',  // можно менять
     },
     message: {
-        text: 'some textsome textsome textsome textsome textsome textsome text', // можно менять
+        text: 'Hello, my friends', // можно менять
         time: '22:00', // можно менять
     },
 }
@@ -32,10 +46,10 @@ export const friendMessage0: MessageType = {
     id: 100,
     user: {
         avatar: avatar, // можно менять
-        name: 'Friend Name', // можно менять
+        name: 'Nikita', // можно менять
     },
     message: {
-        text: 'зеркальное сообщение для тренировки css', // можно менять
+        text: 'Hello, how are you', // можно менять
         time: '22:00', // можно менять
     },
 }
